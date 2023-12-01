6d3fa713206e0f17a33771fa89037f3266e4cff1
Andrea Iacono <aiacono@spotify.com>
2023-12-01 16:57:42 +0100

    day 1 - 2023

---
+++ src/main/kotlin/2023/01.kt
@@ -0,0 +1,38 @@
+package `2023`
+
+import java.io.File
+import kotlin.math.min
+
+val charNumbers = CharArray(10) { (it + 48).toChar() }
+val spelled = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val numbersAndSpelledNumbers = spelled + charNumbers.map { it.toString() }.toList()
+
+fun main() {
+    val lines = File("src/main/resources/2023/01.txt").readLines()
+    println("First puzzle: " + lines.sumOf { getNumber(it) })
+    println("Second puzzle: " + lines.sumOf { getSpelledNumber(it) })
+}
+
+fun getNumber(row: String): Int {
+    val firstDigit = row[row.indexOfAny(charNumbers)].digitToInt()
+    val secondDigit = row[row.lastIndexOfAny(charNumbers)].digitToInt()
+    return firstDigit * 10 + secondDigit
+}
+
+fun getSpelledNumber(row: String): Int {
+
+    val firstIndex = row.indexOfAny(numbersAndSpelledNumbers)
+    val firstDigit = if (row[firstIndex].isDigit()) row[firstIndex].digitToInt() else getSpelledValue(
+        row.substring(firstIndex, min(row.length, firstIndex + 5))
+    )
+    val secondIndex = row.lastIndexOfAny(numbersAndSpelledNumbers)
+    val secondDigit = if (row[secondIndex].isDigit()) row[secondIndex].digitToInt() else getSpelledValue(
+        row.substring(secondIndex, min(row.length , secondIndex + 5))
+    )
+    return firstDigit * 10 + secondDigit
+}
+
+fun getSpelledValue(value: String): Int {
+    spelled.forEach { if (value.startsWith(it)) return spelled.indexOf(it) }
+    return 0
+}
